683c107d5fc81a8ba82887590f9f1890736c4743
MenaVillalobos <jimee69styles@gmail.com>
2023-01-20 14:49:20 -0600

    fixing Sign Up and Log In menu

--- src/main.js
+++ src/main.js
@@ -3,3 +3,33 @@
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+document.getElementById("SignInButtonOption").addEventListener("click", SignUp);
+function SignUp(){
+    const signUpMain=document.getElementById("mainSignUp");
+    signUpMain.classList.add("showMain");
+    signUpMain.classList.remove("hideMain");
+    const logInMain=document.getElementById("mainLogIn"); 
+    logInMain.classList.add("hideMain");
+    logInMain.classList.remove("showMain");
+//Para dar sombreado al seleccionar sign up
+    const signUpSelected=document.getElementById("SignInButtonOption");
+    signUpSelected.classList.add("btnSelected");
+    const logInSelected=document.getElementById("LogInButtonOption");
+    logInSelected.classList.remove("btnSelected");
+}
+
+document.getElementById("LogInButtonOption").addEventListener("click", LogIn);
+function LogIn() {
+    const signUpMain=document.getElementById("mainSignUp");
+    signUpMain.classList.add("hideMain");
+    signUpMain.classList.remove("showMain");
+    const logInMain=document.getElementById("mainLogIn");
+    logInMain.classList.add("showMain");
+    logInMain.classList.remove("hideMain");
+//Para dar sombreado al seleccionar log in
+    const logInSelected=document.getElementById("LogInButtonOption");
+    logInSelected.classList.add("btnSelected");
+    const signInSelected=document.getElementById("SignInButtonOption");
+    signInSelected.classList.remove("btnSelected");
+}
